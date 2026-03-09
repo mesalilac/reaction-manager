5bd0b0f461798d0c7d07b5c9360d0c99e0bfa2c9
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-09 10:39:51 +0300

    create util functions

--- src-tauri/src/utils/fs.rs
+++ src-tauri/src/utils/fs.rs
@@ -1,6 +1,9 @@
 use crate::APP_NAME;
 use std::path::PathBuf;
 
+/// Returns the app data directory.
+///
+/// This is where the database and media are stored
 pub fn get_app_data_dir() -> PathBuf {
     let app_data_dir = directories::BaseDirs::new()
         .expect("Failed to get base dir")
@@ -12,4 +15,80 @@ pub fn get_app_data_dir() -> PathBuf {
     }
 
     app_data_dir
+}
+
+/// Returns the media directory.
+pub fn get_app_media_dir() -> PathBuf {
+    let app_media_dir = get_app_data_dir().join("media");
+
+    if !app_media_dir.exists() {
+        std::fs::create_dir_all(&app_media_dir).expect("Failed to create media dir");
+    }
+
+    app_media_dir
+}
+
+/// Returns the images directory.
+///
+/// Place to store images
+pub fn get_app_images_dir() -> PathBuf {
+    let app_images_dir = get_app_media_dir().join("images");
+
+    if !app_images_dir.exists() {
+        std::fs::create_dir_all(&app_images_dir).expect("Failed to create images dir");
+    }
+
+    app_images_dir
+}
+
+/// Returns the videos directory.
+///
+/// Place to store videos
+pub fn get_app_videos_dir() -> PathBuf {
+    let app_videos_dir = get_app_media_dir().join("videos");
+
+    if !app_videos_dir.exists() {
+        std::fs::create_dir_all(&app_videos_dir).expect("Failed to create videos dir");
+    }
+
+    app_videos_dir
+}
+
+/// Returns the audio directory.
+///
+/// Place to store audio
+pub fn get_app_audio_dir() -> PathBuf {
+    let app_audio_dir = get_app_media_dir().join("audio");
+
+    if !app_audio_dir.exists() {
+        std::fs::create_dir_all(&app_audio_dir).expect("Failed to create audio dir");
+    }
+
+    app_audio_dir
+}
+
+/// Returns the thumbnails directory.
+///
+/// Place to store thumbnails for videos
+pub fn get_app_thumbnails_dir() -> PathBuf {
+    let app_thumbnails_dir = get_app_media_dir().join("thumbnails");
+
+    if !app_thumbnails_dir.exists() {
+        std::fs::create_dir_all(&app_thumbnails_dir).expect("Failed to create thumbnails dir");
+    }
+
+    app_thumbnails_dir
+}
+
+/// Returns the temp directory.
+///
+/// Place to store temp files for downloading
+pub fn get_app_temp_dir() -> PathBuf {
+    let app_temp_dir = get_app_media_dir().join("temp");
+
+    if !app_temp_dir.exists() {
+        std::fs::create_dir_all(&app_temp_dir).expect("Failed to create temp dir");
+    }
+
+    app_temp_dir
 }
