59831de6c8ab0de39a3ee1eb99d62e047b54f0c5
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-14 02:04:38 +0300

    remove total field

--- src-tauri/src/events.rs
+++ src-tauri/src/events.rs
@@ -3,5 +3,4 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, specta::Type, tauri_specta::Event)]
 pub struct FileProcessingProgress {
     pub current: i32,
-    pub total: i32,
 }
